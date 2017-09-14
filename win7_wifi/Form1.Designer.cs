fe971e5fcf132911e97d226a43058e253119abe3
LiMao <limao@iot.chinamobile.com>
2017-09-14 11:45:00 +0800

    update

---
+++ win7_wifi/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace win7_wifi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.st = new System.Windows.Forms.Button();
+            this.en = new System.Windows.Forms.Button();
+            this.nname = new System.Windows.Forms.TextBox();
+            this.psword = new System.Windows.Forms.TextBox();
+            this.wifi_status = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pwd_check = new System.Windows.Forms.CheckBox();
+            this.psword2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // st
+            // 
+            this.st.Location = new System.Drawing.Point(33, 87);
+            this.st.Name = "st";
+            this.st.Size = new System.Drawing.Size(91, 30);
+            this.st.TabIndex = 0;
+            this.st.Text = "建立";
+            this.st.UseVisualStyleBackColor = true;
+            this.st.Click += new System.EventHandler(this.st_Click);
+            // 
+            // en
+            // 
+            this.en.Location = new System.Drawing.Point(160, 87);
+            this.en.Name = "en";
+            this.en.Size = new System.Drawing.Size(91, 30);
+            this.en.TabIndex = 1;
+            this.en.Text = "删除并退出";
+            this.en.UseVisualStyleBackColor = true;
+            this.en.Click += new System.EventHandler(this.en_Click);
+            // 
+            // nname
+            // 
+            this.nname.Location = new System.Drawing.Point(101, 9);
+            this.nname.Name = "nname";
+            this.nname.Size = new System.Drawing.Size(100, 21);
+            this.nname.TabIndex = 2;
+            // 
+            // psword
+            // 
+            this.psword.Location = new System.Drawing.Point(101, 48);
+            this.psword.Name = "psword";
+            this.psword.PasswordChar = '*';
+            this.psword.Size = new System.Drawing.Size(100, 21);
+            this.psword.TabIndex = 3;
+            // 
+            // wifi_status
+            // 
+            this.wifi_status.Location = new System.Drawing.Point(12, 133);
+            this.wifi_status.Multiline = true;
+            this.wifi_status.Name = "wifi_status";
+            this.wifi_status.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.wifi_status.Size = new System.Drawing.Size(268, 130);
+            this.wifi_status.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(29, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "名字：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(29, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "密码：";
+            // 
+            // pwd_check
+            // 
+            this.pwd_check.AutoSize = true;
+            this.pwd_check.Location = new System.Drawing.Point(208, 53);
+            this.pwd_check.Name = "pwd_check";
+            this.pwd_check.Size = new System.Drawing.Size(72, 16);
+            this.pwd_check.TabIndex = 7;
+            this.pwd_check.Text = "显示密码";
+            this.pwd_check.UseVisualStyleBackColor = true;
+            this.pwd_check.CheckedChanged += new System.EventHandler(this.pwd_changed);
+            // 
+            // psword2
+            // 
+            this.psword2.Location = new System.Drawing.Point(101, 49);
+            this.psword2.Name = "psword2";
+            this.psword2.Size = new System.Drawing.Size(100, 21);
+            this.psword2.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3500;
+            this.timer1.Tick += new System.EventHandler(this.time_tickk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 21);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "关于";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.psword2);
+            this.Controls.Add(this.pwd_check);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.wifi_status);
+            this.Controls.Add(this.psword);
+            this.Controls.Add(this.nname);
+            this.Controls.Add(this.en);
+            this.Controls.Add(this.st);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Win7建立无线热点";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button st;
+        private System.Windows.Forms.Button en;
+        private System.Windows.Forms.TextBox nname;
+        private System.Windows.Forms.TextBox psword;
+        private System.Windows.Forms.TextBox wifi_status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox pwd_check;
+        private System.Windows.Forms.TextBox psword2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ win7_wifi/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace win7_wifi
+{
+    public partial class Form1 : Form
+    {
+        //dosCommand Dos命令语句
+        public string cmd(string dosCommand)
+        {
+            return cmd(dosCommand, 30000);
+        }
+        /// <summary>
+        /// 执行DOS命令，返回DOS命令的输出
+        /// </summary>
+        /// <param name="dosCommand">dos命令</param>
+        /// <param name="milliseconds">等待命令执行的时间（单位：毫秒），
+        /// 如果设定为0，则无限等待</param>
+        /// <returns>返回DOS命令的输出</returns>
+        public static string cmd(string command, int seconds)
+        {
+            string output = ""; //输出字符串
+            if (command != null && !command.Equals(""))
+            {
+                Process process = new Process();//创建进程对象
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = "cmd.exe";//设定需要执行的命令
+                startInfo.Arguments = "/C " + command;//“/C”表示执行完命令后马上退出
+                startInfo.UseShellExecute = false;//不使用系统外壳程序启动
+                startInfo.RedirectStandardInput = false;//不重定向输入
+                startInfo.RedirectStandardOutput = true; //重定向输出
+                startInfo.CreateNoWindow = true;//不创建窗口
+                process.StartInfo = startInfo;
+                try
+                {
+                    if (process.Start())//开始进程
+                    {
+                        if (seconds == 0)
+                        {
+                            process.WaitForExit();//这里无限等待进程结束
+                        }
+                        else
+                        {
+                            process.WaitForExit(seconds); //等待进程结束，等待时间为指定的毫秒
+                        }
+                        output = process.StandardOutput.ReadToEnd();//读取进程的输出
+                    }
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    if (process != null)
+                        process.Close();
+                }
+            }
+            return output;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void st_Click(object sender, EventArgs e)
+        {
+            string temp_psword;
+            if (pwd_check.Checked == true)
+            {
+                temp_psword = psword2.Text;
+
+            }
+            else {
+                temp_psword = psword.Text;
+            }
+            if (temp_psword.Length < 8)
+            {
+                MessageBox.Show("密码长度需大于8位~~~", "提示~");
+            }
+            else
+            {
+                wifi_status.Text = cmd("netsh wlan set hostednetwork mode=allow ssid=" + nname.Text + " key=" + temp_psword);
+                wifi_status.Text += cmd("netsh wlan start hostednetwork");
+
+                if (File.Exists("LM.ini"))
+                {
+                    System.IO.StreamWriter lm = new System.IO.StreamWriter("LM.ini");
+                    lm.WriteLine(nname.Text);
+
+                    //密码加密
+                    char[] temp_w = temp_psword.ToCharArray();
+                    temp_w[0] = (char)(temp_w[0] - 1);
+                    temp_w[1] = (char)(temp_w[1] - 5);
+                    temp_w[2] = (char)(temp_w[2] - 3);
+                    temp_w[3] = (char)(temp_w[3] + 1);
+                    temp_w[4] = (char)(temp_w[4] + temp_w[5]);
+                    temp_w[5] = (char)(temp_w[5] + 25 - temp_w[0]);
+                    temp_w[6] = (char)(temp_w[6] - 3);
+                    temp_w[7] = (char)(temp_w[7] + 10);
+                    string temp_ww = new string(temp_w);
+                    lm.WriteLine(temp_ww);
+                    lm.Close();
+                    lm.Dispose();
+                }
+
+
+                st.Enabled = false;
+                psword.Enabled = false;
+                psword2.Enabled = false;
+                nname.Enabled = false;
+                wifi_status.Text += "完成！";
+            }
+        }
+
+        private void en_Click(object sender, EventArgs e)
+        {
+            wifi_status.Text = cmd("netsh wlan stop hostednetwork");
+            wifi_status.Text += cmd("netsh wlan set hostednetwork mode=disallow");
+            wifi_status.Text += "程序即将退出……";
+            timer1.Enabled = true;
+        }
+
+        private void pwd_changed(object sender, EventArgs e)
+        {
+
+            if (pwd_check.Checked == true)
+            {
+                psword2.Visible = true;
+                psword2.Text = psword.Text;
+            }
+            else {
+                psword2.Visible = false;
+                psword.Text = psword2.Text;
+            }
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            psword2.Visible = false;
+            if (File.Exists("LM.ini")) {
+                System.IO.StreamReader test = new System.IO.StreamReader("LM.ini");
+                nname.Text = test.ReadLine();
+
+                //密码解密
+                string temp_jm=test.ReadLine();
+                char[] temp_jmch = temp_jm.ToCharArray();
+                temp_jmch[0] = (char)(temp_jmch[0] + 1);
+                temp_jmch[1] = (char)(temp_jmch[1] + 5);
+                temp_jmch[2] = (char)(temp_jmch[2] + 3);
+                temp_jmch[3] = (char)(temp_jmch[3] - 1);
+                temp_jmch[5] = (char)(temp_jmch[5] - 26 + temp_jmch[0]);
+                temp_jmch[4] = (char)(temp_jmch[4] - temp_jmch[5]);
+                temp_jmch[6] = (char)(temp_jmch[6] + 3);
+                temp_jmch[7] = (char)(temp_jmch[7] - 10);
+                string temp_jmww = new string(temp_jmch);
+                 if (pwd_check.Checked == true)
+            {
+                
+                psword2.Text  = temp_jmww;
+            }
+            else {
+
+                psword.Text = temp_jmww;
+            }
+                 test.Close();
+                 test.Dispose();
+            }
+            else{
+                StreamWriter sw = File.CreateText("LM.ini");
+                sw.WriteLine("Maozi");
+
+                char[] temp_2w = "12345678".ToCharArray();
+                temp_2w[0] = (char)(temp_2w[0] - 1);
+                temp_2w[1] = (char)(temp_2w[1] - 5);
+                temp_2w[2] = (char)(temp_2w[2] - 3);
+                temp_2w[3] = (char)(temp_2w[3] + 1);
+                temp_2w[4] = (char)(temp_2w[4] + temp_2w[5]);
+                temp_2w[5] = (char)(temp_2w[5] + 25 - temp_2w[0]);
+                temp_2w[6] = (char)(temp_2w[6] - 3);
+                temp_2w[7] = (char)(temp_2w[7] + 10);
+                string temp_2ww = new string(temp_2w);
+                sw.WriteLine(temp_2ww);
+                nname.Text = "Maozi";
+                psword.Text = "12345678";
+                sw.Close();
+                sw.Dispose();
+            }
+            
+        }
+
+        private void time_tickk(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Powered by M.Lee 2013.05 供win7用……\r\n基于BSD许可证,重庆邮电大学自动化学院2009级强化班", "O(∩_∩)O关于~");
+            
+           
+
+        }
+    }
+}
